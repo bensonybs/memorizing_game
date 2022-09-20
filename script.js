09166c1391a043af298267d3c872462fd2d3ff91
Benson.YBS <yehbo0921@gmail.com>
2022-09-21 00:06:35 +0800

    Add flipCard function

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
 const Symbols = [
-    'https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png', // 黑桃
-    'https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png', // 愛心
-    'https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png', // 方塊
-    'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png' // 梅花
-    ]
-const view = {
-    getCardElement(index) {
-        const number = this.transformNumber((index % 13) + 1)
-        const symbol = Symbols[Math.floor(index / 13)]
-        return `
-            <div class="card">
-                <p>${number}</p>
-                <img src=${symbol}>
-                <p>${number}</p>
-            </div>`
-    },
-    transformNumber (number) {
-        switch (number) {
-          case 1:
-            return 'A'
-          case 11:
-            return 'J'
-          case 12:
-            return 'Q'
-          case 13:
-            return 'K'
-          default:
-            return number
-        }
-      },
-    displayCards() {
-        const rootElement = document.querySelector("#cards");
-        rootElement.innerHTML = Array.from(Array(52).keys()).map(index => this.getCardElement(index)).join("");    
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png', // 黑桃
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png', // 愛心
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png', // 方塊
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png' // 梅花
+]
+const utility = {
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys())
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1))
+        ;[number[index], number[randomIndex]] = [number[randomIndex], number[index]]
     }
+    return number
+  }
+}
+const view = {
+  flipCard(card) {
+    if(card.classList.contains('back')) {
+      //翻回正面
+      card.classList.remove('back')
+      card.innerHTML = this.getCardContent(Number(card.dataset.index))
+      return
+    }
+    card.classList.add('back')
+    card.innerHTML = null
+  },
+  getCardContent(index) {
+    const number = this.transformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)]
+    return `
+      <p>${number}</p>
+      <img src=${symbol}>
+      <p>${number}</p>`
+  },
+  getCardElement(index) {
+    return `
+      <div data-index="${index}" class="card back"></div>`
+  },
+  transformNumber(number) {
+    switch (number) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return number
+    }
+  },
+  displayCards() {
+    const rootElement = document.querySelector("#cards");
+    rootElement.innerHTML = utility.getRandomNumberArray(52).map(index => this.getCardElement(index)).join("");
+  }
 }
 view.displayCards()
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', event => {
+    view.flipCard(card)
+  })
+})
