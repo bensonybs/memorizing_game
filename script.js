ee8c8d1d0528e2003428ab7f0c0a56557311526a
N000173276 <benson.ye@npc.com.tw>
2022-09-23 17:05:20 +0800

    Project complete: score, tried times, game complete bar, animation.

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ const view = {
         card.innerHTML = this.getCardContent(Number(card.dataset.index))
         return
       }
+      //翻回背面
       card.classList.add('back')
       card.innerHTML = null
     })
@@ -68,6 +69,31 @@ const view = {
   displayCards(indexes) {
     const rootElement = document.querySelector("#cards");
     rootElement.innerHTML = indexes.map(index => this.getCardElement(index)).join("");
+  },
+  renderScore(score) {
+    document.querySelector(".score").textContent = `Score: ${score}`
+  },
+  renderTriedTimes(times) {
+    document.querySelector('.tried').textContent = `You've tried: ${times} times`
+  },
+  appendWrongAnimation(...cards) {
+    cards.map(card => {
+      card.classList.add('wrong')
+      card.addEventListener('animationend', event => {
+        event.target.classList.remove('wrong'), { once: true }
+      })
+    })
+  },
+  showGameFinished() {
+    const div = document.createElement('div')
+    div.classList.add('completed')
+    div.innerHTML = `
+      <p>Complete!</p>
+      <p>Score: ${model.score}</p>
+      <p>You've tried: ${model.triedTimes} times</p>
+    `
+    const header = document.querySelector('#header')
+    header.before(div)
   }
 }
 const controller = {
@@ -86,16 +112,26 @@ const controller = {
         this.currentState = GAME_STATE.SecondCardAwaits
         break;
       case GAME_STATE.SecondCardAwaits:
+        view.renderTriedTimes(++model.triedTimes)
         view.flipCards(card)
         model.revealedCards.push(card)
+        //Check the cards is matched
         if (model.isRevealedCardsMatched()) {
+          view.renderScore(model.score += 10)
           this.currentState = GAME_STATE.CardsMatched
           view.pairCards(...model.revealedCards)
           model.revealedCards = []
+          if (model.score == 260) {
+            console.log('showGameFinished')
+            this.currentState = GAME_STATE.GameFinished
+            view.showGameFinished()
+            return
+          }
           this.currentState = GAME_STATE.FirstCardAwaits
         } else {
           this.currentState = GAME_STATE.CardsMatchFailed
-          setTimeout(this.resetCards , 1000)
+          view.appendWrongAnimation(...model.revealedCards)
+          setTimeout(this.resetCards, 1000)
         }
         break;
     }
@@ -112,6 +148,8 @@ const controller = {
 }
 const model = {
   revealedCards: [],
+  score: 0,
+  triedTimes: 0,
   isRevealedCardsMatched() {
     return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
   }
